41233be5f318b9e957ad3f2b5888cf35f62446b4
Shane D Wilson <me@shanedwilson.com>
2019-03-12 16:10:29 -0500

    working

--- FamilyDictionary/Program.cs
+++ FamilyDictionary/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace FamilyDictionary
 {
@@ -6,7 +7,38 @@ namespace FamilyDictionary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<string, Dictionary<string, string>> myFamily =
+                new Dictionary<string, Dictionary<string, string>>();
+
+            myFamily.Add("wife", new Dictionary<string, string>() {
+                { "name", "Stacye" }, { "age", "51" } });
+
+            myFamily.Add("son", new Dictionary<string, string>() {
+                { "name", "Nazz" }, { "age", "15" } });
+
+            myFamily.Add("sister-in-law", new Dictionary<string, string>() {
+                { "name", "Jennifer" }, { "age", "41" } });
+
+            //myFamily.Add("sister-in-law", new Dictionary<string, string>() {
+            //    { "name", "Dana" }, { "age", "43" } });
+
+            myFamily.Add("sister", new Dictionary<string, string>() {
+                { "name", "Krista" }, { "age", "46" } });
+
+            foreach (var member in myFamily)
+            {
+                foreach(var set in member.Value)
+                {
+                    Console.WriteLine($"{set.Value} is my {member.Key} and is {set.Value} years old.");
+                }
+            }
+
+            foreach (var (key, value) in myFamily)
+            {
+                Console.WriteLine($"{myFamily[key]["name"]} is my {key} and is " +
+                    $"{myFamily[key]["age"]} years old.");
+            }
+            Console.ReadKey();
         }
     }
 }
